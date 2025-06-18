49fdfff35813892edf865ee07f730d1266f9e953
Marcelo <dennermarcelopereira@gmail.com>
2025-06-18 17:50:10 -0300

    exercicio usando format , somando dois numeros entre eles

--- app.py
+++ app.py
@@ -1,13 +1,6 @@
-nome = input('qual seu nome?')
+a = int(input('escolha um número'))
+b = int(input('escolha outro número'))
 
-print(f'Olá {nome} seja bem-vindo ao questionário básico')
+s = a + b
 
-print('Escolha qual seu time de coração:' 'Internacional, ' 'Grêmio,' 'Santos,' 'Palmeiras' )
-
-input('qual seu time de coração?')
-
-print('Ok, você sabe o nome de um jogador deste time: ')
-
-input('se sim, qual seria? caso não saiba escreva, não se aplica: ')
-
-print('Parabéns questionário concluido com sucesso, esperamos que tenha gostado e volta sempre !')
+print('a soma entre {} e {} é: {}'.format(a, b, s))
---
+++ questionário simples/app.py
@@ -0,0 +1,13 @@
+nome = input('qual seu nome?')
+
+print(f'Olá {nome} seja bem-vindo ao questionário básico')
+
+print('Escolha qual seu time de coração:' 'Internacional, ' 'Grêmio,' 'Santos,' 'Palmeiras' )
+
+input('Qual seu time de coração?')
+
+print('Ok, você sabe o nome de um jogador deste time: ')
+
+input('Se sim, qual seria? caso não saiba escreva, não se aplica: ')
+
+print('Parabéns questionário concluido com sucesso, esperamos que tenha gostado e volte sempre !')
