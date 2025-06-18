406a448c7ce89cf1b2c1e36991a37e5f7c251962
Marcelo <dennermarcelopereira@gmail.com>
2025-06-18 15:27:57 -0300

    questionário simples

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
-a = int(input('Escolha u número :'))
-b = int(input('Escolha outro número :'))
-soma = a + b
+nome = input('qual seu nome?')
 
-print(f'a soma entre os dois números escolhidos é : {soma}')
+print(f'Olá {nome} seja bem-vindo ao questionário básico')
+
+print('Escolha qual seu time de coração:' 'Internacional, ' 'Grêmio,' 'Santos,' 'Palmeiras' )
+
+input('qual seu time de coração?')
+
+print('Ok, você sabe o nome de um jogador deste time: ')
+
+input('se sim, qual seria? caso não saiba escreva, não se aplica: ')
+
+print('Parabéns questionário concluido com sucesso, esperamos que tenha gostado e volta sempre !')
